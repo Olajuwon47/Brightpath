39aed488fca4c0f8dc712b43083d461cefb39264
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2026-06-26 04:05:37 +0100

    Delete

--- Frontend/bright-path/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { SpinnerCustom } from "@/components/ui/spinner"
-
-export default function Loading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-white">
-      <SpinnerCustom />
-    </div>
-  )
-}
